fc3813d8bfe9b43a8619c9a918876ab5481c7e2c
Pradeep <pradeepnailwal55@gmail.com>
2023-05-14 15:13:15 +0530

    first commit

---
+++ src/main/java/com/org/main/MakeOurTripProjectAutomation/App.java
@@ -0,0 +1,13 @@
+package com.org.main.MakeOurTripProjectAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
